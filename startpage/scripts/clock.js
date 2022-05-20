cbf150196e7b3c1d782b0755ad3daa6c6c53d9cb
yuno-cchi <fakeymcnoexist@gmail.com>
2022-05-20 23:59:25 +0800

    Startpage foundation set

---
+++ startpage/scripts/clock.js
@@ -0,0 +1,25 @@
+function displayTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    var twelveHour = "AM";
+    // Date() gives us the time in 24 hour notation, so we convert it to 12
+    if (h == 0) {
+        h = 12;
+    }
+    else if (h > 12) {
+        h -= 12;
+        twelveHour = "PM";
+    }
+    // we add padding zeroes for style cuz Date() doesn't do that
+    var hh = (h < 10) ? "0" + h.toString() : h.toString();
+    var mm = (m < 10) ? "0" + m.toString() : m.toString();
+    var ss = (s < 10) ? "0" + s.toString() : s.toString();
+    var timeNow = hh + ":" + mm + ":" + ss + " " + twelveHour;
+    document.getElementById("time").innerHTML = timeNow;
+    // repeat displayTime() every 500ms 
+    var t = setTimeout(function () { displayTime(); }, 500);
+    // i honestly don't know what this means tbh
+}
+displayTime();
---
+++ startpage/scripts/clock.ts
@@ -0,0 +1,31 @@
+function displayTime() {
+    const today = new Date();
+    let h: number = today.getHours();
+    let m: number = today.getMinutes();
+    let s: number = today.getSeconds();
+    let twelveHour: string = "AM";
+
+    // Date() gives us the time in 24 hour notation, so we convert it to 12
+    if(h == 0) {
+        h = 12;
+    } else if (h > 12) {
+        h -= 12;
+        twelveHour = "PM";
+    }
+
+    // we add padding zeroes for style cuz Date() doesn't do that
+
+    let hh: string = (h < 10) ? "0" + h.toString() : h.toString();
+    let mm: string = (m < 10) ? "0" + m.toString() : m.toString();
+    let ss: string = (s < 10) ? "0" + s.toString() : s.toString();
+
+    let timeNow: string = hh + ":" + mm + ":" + ss + " " +  twelveHour;
+
+    document.getElementById("time").innerHTML = timeNow;
+
+    // repeat displayTime() every 500ms 
+    let t = setTimeout(function(){ displayTime() }, 500);
+    // i honestly don't know what this means tbh
+}
+
+displayTime();
