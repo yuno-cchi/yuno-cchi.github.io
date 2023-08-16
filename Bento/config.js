cfd4f1ad1cccdd48e5c945414b9d0e05aa3ecb3d
yuno-cchi <fakeymcnoexist@gmail.com>
2023-08-16 15:50:05 +0800

    new startpage

---
+++ Bento/config.js
@@ -0,0 +1,230 @@
+// ╔╗ ╔═╗╔╗╔╔╦╗╔═╗
+// ╠╩╗║╣ ║║║ ║ ║ ║
+// ╚═╝╚═╝╝╚╝ ╩ ╚═╝
+// ┌─┐┌─┐┌┐┌┌─┐┬┌─┐┬ ┬┬─┐┌─┐┌┬┐┬┌─┐┌┐┌
+// │  │ ││││├┤ ││ ┬│ │├┬┘├─┤ │ ││ ││││
+// └─┘└─┘┘└┘└  ┴└─┘└─┘┴└─┴ ┴ ┴ ┴└─┘┘└┘
+
+const CONFIG = {
+	// ┌┐ ┌─┐┌─┐┬┌─┐┌─┐
+	// ├┴┐├─┤└─┐││  └─┐
+	// └─┘┴ ┴└─┘┴└─┘└─┘
+
+	// General
+	name: 'Yuno',
+	imageBackground: true,
+	openInNewTab: true,
+	twelveHourFormat: false,
+
+	// Greetings
+	greetingMorning: 'Good morning!',
+	greetingAfternoon: 'Good afternoon,',
+	greetingEvening: 'Good evening,',
+	greetingNight: 'Go to Sleep!',
+
+	// Layout
+	bentoLayout: 'lists', // 'bento', 'lists', 'buttons'
+
+	// Weather
+	weatherKey: '2565af857711ae887a0c4df89dbfe607', // Write here your API Key
+	weatherIcons: 'White', // 'Onedark', 'Nord', 'Dark', 'White'
+	weatherUnit: 'C', // 'F', 'C'
+	language: 'en', // More languages in https://openweathermap.org/current#multi
+
+	trackLocation: false, // If false or an error occurs, the app will use the lat/lon below
+	defaultLatitude: '1.348',
+	defaultLongitude: '103.683',
+
+	// Autochange
+	autoChangeTheme: true,
+
+	// Autochange by OS
+	changeThemeByOS: true,
+
+	// Autochange by hour options (24hrs format, string must be in: hh:mm)
+	changeThemeByHour: false,
+	hourDarkThemeActive: '18:30',
+	hourDarkThemeInactive: '07:00',
+
+	// ┌┐ ┬ ┬┌┬┐┌┬┐┌─┐┌┐┌┌─┐
+	// ├┴┐│ │ │  │ │ ││││└─┐
+	// └─┘└─┘ ┴  ┴ └─┘┘└┘└─┘
+
+	firstButtonsContainer: [
+		{
+			id: '1',
+			name: 'Github',
+			icon: 'github',
+			link: 'https://github.com/',
+		},
+		{
+			id: '2',
+			name: 'Mail',
+			icon: 'mail',
+			link: 'https://mail.protonmail.com/',
+		},
+		{
+			id: '3',
+			name: 'Todoist',
+			icon: 'trello',
+			link: 'https://todoist.com',
+		},
+		{
+			id: '4',
+			name: 'Calendar',
+			icon: 'calendar',
+			link: 'https://calendar.google.com/calendar/r',
+		},
+		{
+			id: '5',
+			name: 'Reddit',
+			icon: 'glasses',
+			link: 'https://reddit.com',
+		},
+		{
+			id: '6',
+			name: 'Odysee',
+			icon: 'youtube',
+			link: 'https://odysee.com/',
+		},
+	],
+
+	secondButtonsContainer: [
+		{
+			id: '1',
+			name: 'Music',
+			icon: 'headphones',
+			link: 'https://open.spotify.com',
+		},
+		{
+			id: '2',
+			name: 'twitter',
+			icon: 'twitter',
+			link: 'https://twitter.com/',
+		},
+		{
+			id: '3',
+			name: 'bot',
+			icon: 'bot',
+			link: 'https://discord.com/app',
+		},
+		{
+			id: '4',
+			name: 'Amazon',
+			icon: 'shopping-bag',
+			link: 'https://amazon.com/',
+		},
+		{
+			id: '5',
+			name: 'Hashnode',
+			icon: 'pen-tool',
+			link: 'https://hashnode.com/',
+		},
+		{
+			id: '6',
+			name: 'Figma',
+			icon: 'figma',
+			link: 'https://figma.com/',
+		},
+	],
+
+	// ┬  ┬┌─┐┌┬┐┌─┐
+	// │  │└─┐ │ └─┐
+	// ┴─┘┴└─┘ ┴ └─┘
+
+	// First Links Container
+	firstlistsContainer: [
+		{
+			icon: 'trello',
+			id: '1',
+			links: [
+				{
+					name: 'NTULearn',
+					link: 'https://ntulearn.ntu.edu.sg/',
+				},
+				{
+					name: 'STARS',
+					link: 'https://wish.wis.ntu.edu.sg/pls/webexe/ldap_login.login?w_url=https://wish.wis.ntu.edu.sg/pls/webexe/aus_stars_planner.main',
+				},
+				{
+					name: 'ChatGPT',
+					link: 'https://chat.openai.com/',
+				},
+				{
+					name: 'WolframAlpha',
+					link: 'https://www.wolframalpha.com/',
+				},
+			],
+		},
+		{
+			icon: 'bot',
+			id: '2',
+			links: [
+				{
+					name: 'Mail (Main)',
+					link: 'https://mail.google.com/mail/u/0/',
+				},
+				{
+					name: 'Mail (Serious)',
+					link: 'https://mail.google.com/mail/u/1/',
+				},
+				{
+					name: 'NTU OneDrive',
+					link: 'https://entuedu-my.sharepoint.com/',
+				},
+				{
+					name: 'Calendar',
+					link: 'https://calendar.google.com/',
+				},
+			],
+		},
+	],
+
+	// Second Links Container
+	secondListsContainer: [
+		{
+			icon: 'binary',
+			id: '1',
+			links: [
+				{
+					name: 'Twitter',
+					link: 'https://www.twitter.com/',
+				},
+				{
+					name: 'Reddit',
+					link: 'https://www.reddit.com/',
+				},
+				{
+					name: 'Chess.com',
+					link: 'https://www.chess.com/',
+				},
+				{
+					name: 'Lichess',
+					link: 'https://www.lichess.org/',
+				},
+			],
+		},
+		{
+			icon: 'binary',
+			id: '2',
+			links: [
+				{
+					name: 'GBFWiki',
+					link: 'https://gbf.wiki//',
+				},
+				{
+					name: 'BBCF Dustloop',
+					link: 'https://www.dustloop.com/wiki/index.php?title=BBCF',
+				},
+				{
+					name: 'Monkeytype',
+					link: 'https://monkeytype.com/',
+				},
+				{
+					name: 'GitHub',
+					link: 'https://github.com/',
+				},
+			],
+		},
+	],
+};
--- startpage2/launchpad/src/pages/index.js
+++ startpage2/launchpad/src/pages/index.js
@@ -1,5 +1,31 @@
 import * as React from "react"
+import '../sass/index.scss'
+import { Temporal, toTemporalInstant } from "@js-temporal/polyfill"
+Date.prototype.toTemporalInstant = toTemporalInstant;
 
 export default function Home() {
-  return <div>Hello world!</div>
+
+  const name = 'Yuno';
+  return (
+  <div id='content'>
+    <div id='left-half'>
+      <h1 id='clock'>{now()}</h1>
+      <h2 id='greeting'>Hello, {name}</h2>
+      <h2>{time}</h2>
+      <h2>{}</h2>
+    </div>
+    <div id='right-half'>
+    </div>
+  </div>
+  )
+}
+
+const now = () => {
+  // TODO
+  return Temporal.Now.plainTimeISO().toString;
+}
+
+const UpdateTodo = () => {
+  // TODO
+  return;
 }
